9d14f14bacea8c552bdd7346efc43117daa2cfb8
W. Max Lees <max.lees@gmail.com>
2017-04-22 16:17:05 -0600

    Converted to port 80

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ var config = {
     app: {
       name: 'server'
     },
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 80,
   },
 
   test: {
@@ -16,7 +16,7 @@ var config = {
     app: {
       name: 'server'
     },
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 80,
   },
 
   production: {
@@ -24,7 +24,7 @@ var config = {
     app: {
       name: 'server'
     },
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 80,
   }
 };
 

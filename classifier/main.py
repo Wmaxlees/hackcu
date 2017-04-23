98e4c1ea51d089986cb357b7c41314503c11dee1
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-04-22 23:58:30 -0600

    Tuned hmm

--- classifier/main.py
+++ classifier/main.py
@@ -58,7 +58,7 @@ def main (argv):
     # training_data = load_mnist_training_data(argv[2])
     training_data = np.load(argv[2])
     
-    clf = MLPClassifier(max_iter=400, learning_rate_init=0.003, solver='sgd', verbose=True, alpha=1e-6, hidden_layer_sizes=(10000, 3), random_state=1, activation='tanh')
+    clf = MLPClassifier(max_iter=400, learning_rate_init=0.003, verbose=True, alpha=1e-6, hidden_layer_sizes=(20000, 3), random_state=1, activation='relu')
     clf.fit(training_data, training_labels)  
 
     #filename = 'test/t10k-labels-idx1-ubyte'
